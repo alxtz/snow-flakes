f1e58794fa6b773882d9bacbb96340744812a89a
Alex Tzeng <alxtz.tw@gmail.com>
2017-12-25 17:28:02 +0800

    init

---
+++ spec/snow-flakes-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SnowFlakesView from '../lib/snow-flakes-view';
+
+describe('SnowFlakesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
